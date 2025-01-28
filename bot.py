eafd2cbcf4d2def468ec89cb6e200fcfb7ebca7e
MelinayThePanda <middevrimci@gmail.com>
2025-01-28 19:20:27 +0300

    bot.py değiştirildi

--- bot.py
+++ bot.py
@@ -1,38 +1,28 @@
 import discord
 from discord.ext import commands
-from config import token  # Botun tokenini config dosyasından içe aktarma
+from config import token
 
 intents = discord.Intents.default()
-intents.members = True  # Botun kullanıcılarla çalışmasına ve onları banlamasına izin verir
+ 
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'Giriş yapıldı:  {bot.user.name}')
+    print(f'We have logged in as {client.user}')
 
-@bot.command()
-async def start(ctx):
-    await ctx.send("Merhaba! Ben bir sohbet yöneticisi botuyum!")
-
-@bot.command()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member = None):
-    if member:
-        if ctx.author.top_role <= member.top_role:
-            await ctx.send("Eşit veya daha yüksek rütbeli bir kullanıcıyı banlamak mümkün değildir!")
-        else:
-            await ctx.guild.ban(member)
-            await ctx.send(f"Kullanızı {member.name} banlandı")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith(client.command_prefix):
+        await client.process_commands(message)
     else:
-        await ctx.send("Bu komut banlamak istediğiniz kullanıcıyı işaret etmelidir. Örneğin: `!ban @user`")
+        await message.channel.send(message.content)
 
-@ban.error
-async def ban_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("Bu komutu çalıştırmak için yeterli izniniz yok.")
-    elif isinstance(error, commands.MemberNotFound):
-        await ctx.send("Kullanıcı bulunamadı!")
+@client.command()
+async def about(ctx):
+    await ctx.send('Bu discord.py kütüphanesi ile oluşturulmuş echo-bot!')
 
-bot.run(token)
+client.run(token)
